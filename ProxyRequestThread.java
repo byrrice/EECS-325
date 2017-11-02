dc0a1b98b8c22960e2c1dc16b336a0ca28a613dc
byrrice <32532726+byrrice@users.noreply.github.com>
2017-11-01 22:05:26 -0400

    Proxy for Project 1

--- ProxyRequestThread.java
+++ ProxyRequestThread.java
@@ -1,38 +1,61 @@
 import java.net.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
 
 public class ProxyRequestThread extends Thread {
 
-    private static Hashtable<String, InetAddress> cache = new Hashtable<String, InetAddress>();
+    //Initialize server and client socket as well as byte buffer and HashMap
     private Socket server = null;
     private Socket client = null;
     private byte[] request = new byte[2048];
 
-    public ProxyRequestThread(Socket client)
-    {
+    //Constructor that takes in client socket
+    public ProxyRequestThread(Socket client) {
         this.client = client;
     }
 
+    //run method that automatically intializes with .start() due to extending Thread
     @Override
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
             //Writing from client to server
             DataInputStream from_client = new DataInputStream(client.getInputStream());
             from_client.read(request, 0, 2048);
 
+            //Close the connection submessage
             request = closeConnection(request);
-            //get serverName to connect to said host
-            String serverName = getServerName(request);
-            //InetAddress address = getHostName(serverName);
-            //System.out.println(address);
-            //InetAddress hostname = getHostName(serverName);
+
+            //get serverName to connect to server
+            String hostName = getHostName(request);
+
+            //Intialize hostname and DNSCachetableEntry
+            InetAddress hostAddress;
+            DNSCacheTableEntry cacheEntry;
 
 
-            server = new Socket(serverName, 80);
+            if (proxyd.cache.containsKey(hostName)){
+
+                //check if entry is more than 30 seconds old
+                if ((cacheEntry = proxyd.cache.get(hostName)).getTime() < 30){
+
+                    //If it is less, take the address and return that you have found it
+                    hostAddress = cacheEntry.getHostName();
+                    System.out.println("Cache hit: " + hostName);
+                    server = new Socket(hostAddress, 80);
+                }
+                else{
+                    //It expired, so get the address and remove the old entry
+                    hostAddress = InetAddress.getByName(hostName);
+                    proxyd.cache.remove(hostName);
+                    proxyd.cache.put(hostName, new DNSCacheTableEntry(hostAddress));
+                    server = new Socket(hostName, 80);
+                }
+            } else {
+                //it was never in the cache, so put it in
+                hostAddress = InetAddress.getByName(hostName);
+                proxyd.cache.put(hostName, new DNSCacheTableEntry(hostAddress));
+                server = new Socket(hostName, 80);
+            }
 
             //write to server
             DataOutputStream to_server = new DataOutputStream(server.getOutputStream());
@@ -41,42 +64,47 @@ public class ProxyRequestThread extends Thread {
 
             // Send response from server to client
             (new Thread(new ProxyResponseThread(server, client))).start();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private String getServerName(byte[] request) {
-        String serverName = "";
-        String string = new String(request, StandardCharsets.UTF_8);
+    //returns the serverName without the Host: in front of it
+    private String getHostName(byte[] message) {
+        String hostName = "";
+        String[] serverParts;
+        String string = new String(message, StandardCharsets.UTF_8);
         String[] parts = string.split("\r\n");
         int i = 0;
         while (i < parts.length) {
-            if (parts[i].contains("Host: ")){
-                serverName = parts[i].replaceAll("Host: ", "");
-                System.out.println(serverName);
+            if (parts[i].contains("Host: ")) {
+                hostName = parts[i].replaceAll("Host: ", "");
+                serverParts = hostName.split(":");
+                hostName = serverParts[0];
+                System.out.println(serverParts[0]);
             }
             i++;
         }
-        return serverName;
+        return hostName;
     }
-
-
-
-
-
 
     //shoutout to https://stackoverflow.com/questions/18571223/how-to-convert-java-string-into-byte
-    private byte[] closeConnection(byte[] message){
-        //convert from byte to String
-        String output = new String(message, StandardCharsets.UTF_8);
-        output.replaceAll("Connection: keep-alive", "Connection: close");
-        output.replaceAll("Proxy-connection: keep-alive","Connection: close" );
-        //back to bytes
-        return output.getBytes(StandardCharsets.UTF_8);
+    //writes the Connection: close in place of Connection: keep-alive or Proxy-connection: keep-alive
+    private byte[] closeConnection(byte[] message) {
+        String string = new String(message, StandardCharsets.UTF_8);
+        String[] parts = string.split("\r\n");
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < parts.length) {
+            System.out.println(parts[i]);
+            if (parts[i].contains("Connection: keep-alive") || parts[i].contains("Proxy-Connection: keep-alive")) {
+                sb.append("Connection: close" + "\r\n");
+            }
+            else
+                sb.append(parts[i] + "\r\n");
+            i++;
+        }
+        return sb.toString().getBytes(StandardCharsets.UTF_8);
     }
-
-
 }
+
